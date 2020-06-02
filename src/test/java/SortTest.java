3c7e1d560a250147fd4a52a5e55a05340eacd1b3
sserdyuk <sserdyuk@fun.co>
2020-06-02 11:45:08 +0300

    Extract tests

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -12,22 +12,25 @@ class SortTest {
 
     private static final List<String> dataTypes = List.of("0.random", "1.digits", "2.sorted", "3.revers");
 
-    private static final List<Sort> sortTypes = List.of(
-        new BubbleSort(),
-        new HeapSort(),
-        new InsertionSort(),
-        new SelectionSort(),
-        new ShellSort(),
-        new ShellSort10()
+    private static final List<TestCase> testCases = List.of(
+        TestCase.of(new BubbleSort(), 6),
+        TestCase.of(new InsertionSort(), 6),
+        TestCase.of(new SelectionSort(), 6),
+        TestCase.of(new HeapSort(), 8),
+        TestCase.of(new ShellSort(), 8),
+        TestCase.of(new ShellSort10(), 8)
     );
 
     @Test
     public void testSorts() throws IOException {
-        for (Sort sortType : sortTypes) {
+        for (TestCase testCase : testCases) {
+            Sort sortType = testCase.sortType;
+            int numCase = testCase.numCase;
             System.out.println(sortType.getClass().getName());
+
             for (String data : dataTypes) {
                 System.out.println("data: " + data);
-                for (int i = 0; i < 7; i++) {
+                for (int i = 0; i < numCase; i++) {
                     long start = System.currentTimeMillis();
                     Pair<Integer, int[]> in = in(data, i);
                     int[] out = out(data, i);
@@ -36,8 +39,18 @@ class SortTest {
                     System.out.println(String.format("  size: %7s time: %s", in.getValue().length, System.currentTimeMillis() - start));
                 }
             }
-            System.out.println("---------------------------");
         }
+        System.out.println("---------------------------");
+    }
+
+    private int getTestCase(Sort sortType) {
+        int testCase = 8;
+        if ((sortType instanceof BubbleSort) ||
+            (sortType instanceof InsertionSort) ||
+            (sortType instanceof SelectionSort)) {
+            testCase = 6;
+        }
+        return testCase;
     }
 
     public static Pair<Integer, int[]> in(String type, int num) throws IOException {
@@ -52,5 +65,20 @@ class SortTest {
         String path = ClassLoader.getSystemResource("sorting-tests").getPath();
         List<String> args = Files.readAllLines(Path.of(path + "/" + type + "/" + "test." + num + ".out"));
         return Arrays.stream(args.get(0).split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static class TestCase {
+
+        private final Sort sortType;
+        private final int numCase;
+
+        private TestCase(Sort sortType, int numCase) {
+            this.sortType = sortType;
+            this.numCase = numCase;
+        }
+
+        public static TestCase of(Sort sortType, int numCase) {
+            return new TestCase(sortType, numCase);
+        }
     }
 }
